4101c3f68756a16f8bcd66f1bd1be6197fbddbb8
Goyo <goyodiaz@gmail.com>
2011-04-26 23:46:07 +0200

    Add translation support, better indent, add Goyo as author.

--- points2one/points2one_plugin.py
+++ points2one/points2one_plugin.py
@@ -2,7 +2,9 @@
 # 
 # Points2One
 # Copyright (C) 2010 Pavol Kapusta
+# Copyright (C) 2011 Pavol Kapusta & Goyo Diaz
 # pavol.kapusta@gmail.com
+# goyodiaz@gmail.com
 # 
 #-----------------------------------------------------------
 # 
@@ -24,6 +26,8 @@
 # 
 #---------------------------------------------------------------------
 
+import os.path
+
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from qgis.core import *
@@ -31,28 +35,39 @@ from qgis.core import *
 import resources
 import points2one_gui
 
+
 class points2one:
 
-  def __init__(self, iface):
-    self.iface = iface
+    def __init__(self, iface):
+        self.iface = iface
+        self.load_translation()
+
+    def load_translation(self):
+        ## Initialise the translation environment.
+        locale = QSettings().value("locale/userLocale").toString()
+        locale_path = os.path.join(os.path.dirname(__file__), 'i18n',
+            ''.join(['points2one_', unicode(locale), '.qm']))
+        if QFileInfo(locale_path).exists():
+            self.translator = QTranslator()
+            self.translator.load(locale_path)
+            if qVersion() > '4.3.3':
+                QCoreApplication.installTranslator(self.translator)
 
 
-  def initGui(self):
-    # create action 
-    self.action = QAction(QIcon(':/plugins/points2one/points2one.png'), 'Points2One', self.iface.mainWindow())
-    self.action.setWhatsThis('Tool for creating polygon or polyline from ordered points. Does not deal with rings and parts')
-    QObject.connect(self.action, SIGNAL('triggered()'), self.run)
-    # add toolbar button and menu item
-    self.iface.addToolBarIcon(self.action)
-    self.iface.addPluginToMenu('&Points2One', self.action)
+    def initGui(self):
+        # create action 
+        self.action = QAction(QIcon(':/plugins/points2one/points2one.png'), 'Points2One', self.iface.mainWindow())
+        self.action.setWhatsThis('Tool for creating polygon or polyline from ordered points. Does not deal with rings and parts')
+        QObject.connect(self.action, SIGNAL('triggered()'), self.run)
+        # add toolbar button and menu item
+        self.iface.addToolBarIcon(self.action)
+        self.iface.addPluginToMenu('&Points2One', self.action)
 
+    def unload(self):
+        # remove the plugin menu item and icon
+        self.iface.removePluginMenu('&Points2One',self.action)
+        self.iface.removeToolBarIcon(self.action)
 
-  def unload(self):
-    # remove the plugin menu item and icon
-    self.iface.removePluginMenu('&Points2One',self.action)
-    self.iface.removeToolBarIcon(self.action)
-
-
-  def run(self):
-    dialog = points2one_gui.points2One(self.iface)
-    dialog.exec_()
+    def run(self):
+        dialog = points2one_gui.points2One(self.iface)
+        dialog.exec_()
