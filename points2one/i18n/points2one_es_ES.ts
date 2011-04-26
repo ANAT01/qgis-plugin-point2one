1465781b92a69452eb9171fd8115f67caf22dd08
Goyo <goyodiaz@gmail.com>
2011-04-26 23:38:22 +0200

    Add i18n directory and french and spanish translations.

---
+++ points2one/i18n/points2one_es_ES.ts
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="frmPoints2One.ui" line="20"/>
+        <source>Points2One</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="29"/>
+        <source>Input point layer</source>
+        <translation>Capa de puntos de entrada</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="48"/>
+        <source>Output geometries</source>
+        <translation>Geometría de la salida</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="56"/>
+        <source>Polygons</source>
+        <translation>Polígonos</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="69"/>
+        <source>Lines</source>
+        <translation>Líneas</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="87"/>
+        <source>Multiple processing</source>
+        <translation>Multiproceso</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="93"/>
+        <source>Create output features based on input field </source>
+        <translation>Crear entidades de salida según campo de entrada</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="113"/>
+        <source>Sort points by this field</source>
+        <translation>Ordenar puntos por este campo</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="123"/>
+        <source>Output shapefile</source>
+        <translation>Archivo shape de salida</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="134"/>
+        <source>Browse</source>
+        <translation>Examinar</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="148"/>
+        <source>Encoding</source>
+        <translation>Codificación</translation>
+    </message>
+</context>
+<context>
+    <name>parent</name>
+    <message>
+        <location filename="points2one_gui.py" line="202"/>
+        <source>Save output shapefile</source>
+        <translation>Guardar archivo shape de salida</translation>
+    </message>
+</context>
+<context>
+    <name>points2One</name>
+    <message>
+        <location filename="points2one_gui.py" line="92"/>
+        <source>Please specify an input layer</source>
+        <translation>Especifique una capa de entrada</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="99"/>
+        <source>Polyline: Please select input layer with at least 2 points</source>
+        <translation>Línea: Seleccione una capa de entrada con al menos 2 puntos</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="101"/>
+        <source>Polygon: Please select input layer with at least 3 points</source>
+        <translation>Polígono: Seleccione una capa de entrada con al menos 2 puntos</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="103"/>
+        <source>Please define specific input field</source>
+        <translation>Defina el campo de entrada</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="105"/>
+        <source>Please specify output shapefile</source>
+        <translation>Especifique el archivo shape de salida</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="121"/>
+        <source>Unable to delete existing shapefile.</source>
+        <translation>No se puede borrar el archivo shape existente.</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="129"/>
+        <source>Output shapefile created</source>
+        <translation>Archivo shape de salida creado</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="131"/>
+        <source>There were some issues, maybe some features could not be created.</source>
+        <translation>Ocurrieron algunos problemas, puede que algunas entidades no se hayan podido crear.</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="137"/>
+        <source>Created output shapefile:</source>
+        <translation>Archivo shape de salida creado:</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="139"/>
+        <source>Would you like to add the new layer to the TOC?</source>
+        <translation>Quiere añadir la nueva capa al mapa?</translation>
+    </message>
+</context>
+</TS>
---
+++ points2one/i18n/points2one_fr_FR.ts
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="frmPoints2One.ui" line="20"/>
+        <source>Points2One</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="29"/>
+        <source>Input point layer</source>
+        <translation>Couche de points</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="48"/>
+        <source>Output geometries</source>
+        <translation>Type de géométries en sortie</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="56"/>
+        <source>Polygons</source>
+        <translation>Polygones</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="69"/>
+        <source>Lines</source>
+        <translation>Lignes</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="87"/>
+        <source>Multiple processing</source>
+        <translation>Traitement multiple</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="93"/>
+        <source>Create output features based on input field </source>
+        <translation>Créer les entités en se basant sur un champ d&apos;attribut</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="113"/>
+        <source>Sort points by this field</source>
+        <translation>Trier les points en utilisant ce champ</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="123"/>
+        <source>Output shapefile</source>
+        <translation>Shapefile en sortie</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="134"/>
+        <source>Browse</source>
+        <translation>Parcourir</translation>
+    </message>
+    <message>
+        <location filename="frmPoints2One.ui" line="148"/>
+        <source>Encoding</source>
+        <translation>Codage de caractère</translation>
+    </message>
+</context>
+<context>
+    <name>parent</name>
+    <message>
+        <location filename="points2one_gui.py" line="202"/>
+        <source>Save output shapefile</source>
+        <translation>Enregistrer le shapefile en sortie</translation>
+    </message>
+</context>
+<context>
+    <name>points2One</name>
+    <message>
+        <location filename="points2one_gui.py" line="92"/>
+        <source>Please specify an input layer</source>
+        <translation>Veuillez spécifier une couche en entrée</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="99"/>
+        <source>Polyline: Please select input layer with at least 2 points</source>
+        <translation>Polyligne : Veuillez sélectionner une couche en entrée ayant au moins 2 points à relier</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="101"/>
+        <source>Polygon: Please select input layer with at least 3 points</source>
+        <translation>Polygone : Veuillez sélectionner une couche en entrée ayant au moins 3 points à relier</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="103"/>
+        <source>Please define specific input field</source>
+        <translation>Veuillez définir le champ spécifique</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="105"/>
+        <source>Please specify output shapefile</source>
+        <translation>Veuillez indiquer le shapefile en sortie</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="121"/>
+        <source>Unable to delete existing shapefile.</source>
+        <translation>Impossible de supprimer le shapefile existant.</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="129"/>
+        <source>Output shapefile created</source>
+        <translation>Fichier shapefile créé</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="131"/>
+        <source>There were some issues, maybe some features could not be created.</source>
+        <translation>Il y a eu quelques soucis, peut-être certaines entités n&apos;ont pu être créées.</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="137"/>
+        <source>Created output shapefile:</source>
+        <translation>Shapefile en sortie :</translation>
+    </message>
+    <message>
+        <location filename="points2one_gui.py" line="139"/>
+        <source>Would you like to add the new layer to the TOC?</source>
+        <translation>Voulez-vous ajouter cette nouvelle couche à votre projet ?</translation>
+    </message>
+</context>
+</TS>
