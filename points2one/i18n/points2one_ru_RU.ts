13841fdf71aaee875fedaa715c300c3da08835d4
ANAT01 <anatoly@tobgeo.ru>
2015-03-08 05:05:11 +0500

    Add Russian translation

---
+++ points2one/i18n/points2one_ru_RU.ts
@@ -0,0 +1,117 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../frmPoints2One.ui" line="20"/>
+        <source>Points2One</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../frmPoints2One.ui" line="29"/>
+        <source>Input point layer</source>
+        <translation>Выбрать точечный слой</translation>
+    </message>
+    <message>
+        <location filename="../frmPoints2One.ui" line="48"/>
+        <source>Output geometries</source>
+        <translation>Геометрия</translation>
+    </message>
+    <message>
+        <location filename="../frmPoints2One.ui" line="56"/>
+        <source>Polygons</source>
+        <translation>Полигоны</translation>
+    </message>
+    <message>
+        <location filename="../frmPoints2One.ui" line="69"/>
+        <source>Lines</source>
+        <translation>Линии</translation>
+    </message>
+    <message>
+        <location filename="../frmPoints2One.ui" line="85"/>
+        <source>Closed</source>
+        <translation>Замкнуть</translation>
+    </message>
+    <message>
+        <location filename="../frmPoints2One.ui" line="103"/>
+        <source>Multiple processing</source>
+        <translation>Многократная обработка</translation>
+    </message>
+    <message>
+        <location filename="../frmPoints2One.ui" line="109"/>
+        <source>Create output features based on input field </source>
+        <translation>Создать объекты на основе поля</translation>
+    </message>
+    <message>
+        <location filename="../frmPoints2One.ui" line="129"/>
+        <source>Sort points by this field</source>
+        <translation>Сортировать точки по выбранному полю</translation>
+    </message>
+    <message>
+        <location filename="../frmPoints2One.ui" line="139"/>
+        <source>Output shapefile</source>
+        <translation>Сохранить в shape-файл</translation>
+    </message>
+    <message>
+        <location filename="../frmPoints2One.ui" line="150"/>
+        <source>Browse</source>
+        <translation>Обзор</translation>
+    </message>
+    <message>
+        <location filename="../frmPoints2One.ui" line="164"/>
+        <source>Encoding</source>
+        <translation>Кодировка</translation>
+    </message>
+</context>
+<context>
+    <name>parent</name>
+    <message>
+        <source>Save output shapefile</source>
+        <translation type="obsolete">Сохранить shape-файл</translation>
+    </message>
+</context>
+<context>
+    <name>points2One</name>
+    <message>
+        <source>Please specify an input layer</source>
+        <translation type="obsolete">Veuillez spécifier une couche en entrée</translation>
+    </message>
+    <message>
+        <source>Polyline: Please select input layer with at least 2 points</source>
+        <translation type="obsolete">Polyligne : Veuillez sélectionner une couche en entrée ayant au moins 2 points à relier</translation>
+    </message>
+    <message>
+        <source>Polygon: Please select input layer with at least 3 points</source>
+        <translation type="obsolete">Polygone : Veuillez sélectionner une couche en entrée ayant au moins 3 points à relier</translation>
+    </message>
+    <message>
+        <source>Please define specific input field</source>
+        <translation type="obsolete">Veuillez définir le champ spécifique</translation>
+    </message>
+    <message>
+        <source>Please specify output shapefile</source>
+        <translation type="obsolete">Veuillez indiquer le shapefile en sortie</translation>
+    </message>
+    <message>
+        <source>Unable to delete existing shapefile.</source>
+        <translation type="obsolete">Impossible de supprimer le shapefile existant.</translation>
+    </message>
+    <message>
+        <source>Output shapefile created</source>
+        <translation type="obsolete">Fichier shapefile créé</translation>
+    </message>
+    <message>
+        <source>There were some issues, maybe some features could not be created.</source>
+        <translation type="obsolete">Il y a eu quelques soucis, peut-être certaines entités n&apos;ont pu être créées.</translation>
+    </message>
+    <message>
+        <source>Created output shapefile:</source>
+        <translation type="obsolete">Shapefile en sortie :</translation>
+    </message>
+    <message>
+        <source>Would you like to add the new layer to the TOC?</source>
+        <translation type="obsolete">Voulez-vous ajouter cette nouvelle couche à votre projet ?</translation>
+    </message>
+</context>
+</TS>
